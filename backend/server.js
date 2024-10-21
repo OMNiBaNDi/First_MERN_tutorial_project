ab1926f6ecde085d9f21feb4f8a54359bf9fd475
Espen <espen.l@hotmail.com>
2024-10-21 04:05:18 +0200

    initial commit

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+import express, { json } from 'express';
+import dotenv from 'dotenv';
+import { connectDB } from './config/db.js';
+
+import productRoutes from "./routes/product.route.js";
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json()); //allows us to accepts json data in the req.body
+
+app.use("/api/products", productRoutes);
+
+
+app.listen(5000, () => {
+    connectDB();
+    console.log("Server started at http://localhost:5000")
+});
+
